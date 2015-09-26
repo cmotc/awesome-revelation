eb1cec83912935320a89a689706daf34aaeef4cd
dyngar <dyngar@dyngar.local>
2015-09-26 14:06:04 -0400

    better debian package

--- debian.sh
+++ debian.sh
@@ -15,20 +15,22 @@ DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
 DEBPACKAGENAME=$DEBFOLDER\_$DEBVERSION
 
 rm -rf $DEBFOLDERNAME
-# Create your scripts source dir
+#Create your scripts source dir
 mkdir $DEBFOLDERNAME
 
 # Copy your script to the source dir
-cp -R $SOURCEBINPATH/ $DEBFOLDERNAME/
+cp * $DEBFOLDERNAME/
 cd $DEBFOLDERNAME
 
 pwd
 
 # Create the packaging skeleton (debian/*)
-dh_make -s --indep --createorig 
+dh_make -s --indep --createorig
 
-mkdir -p debian/tmp/usr
-cp -R $SOURCEBINPATH debian/tmp/usr
+mkdir -p debian/tmp/usr/share/doc/$DEBFOLDER/
+mkdir -p debian/tmp/etc/xdg/revelation
+cp init.lua debian/tmp/etc/xdg/revelation
+cp README.md debian/tmp/usr/share/doc/$DEBFOLDER/
 
 # Remove make calls
 grep -v makefile debian/rules > debian/rules.new 
@@ -39,10 +41,22 @@ mv debian/rules.new debian/rules
 echo $SOURCEBIN etc/xdg/awesome/revelation > debian/install 
 echo $SOURCEDOC usr/share/doc/$DEBFOLDER >> debian/install
 
-#echo "gsettings set org.gnome.desktop.session session-name awesome-gnome
-#dconf write /org/gnome/settings-daemon/plugins/cursor/active false
-#gconftool-2 --type bool --set /apps/gnome_settings_daemon/plugins/background/active false
-#" > debian/postinst
+echo "Source: $DEBFOLDER
+Section: unknown
+Priority: optional
+Maintainer: cmotc <cmotc@openmailbox.org>
+Build-Depends: debhelper (>= 9)
+Standards-Version: 3.9.5
+Homepage: <insert the upstream URL, if relevant>
+#Vcs-Git: git@github.com:cmotc/awesome-revelation
+#Vcs-Browser: https://www.github.com/cmotc/awesome-revelation
+
+Package: $DEBFOLDER
+Architecture: all
+Depends: awesome (>= 3.5), \${misc:Depends}
+Description: Show all clients all screens in Awesome window manager
+ Displays all clients on all screens at once in Awesome Window Manager." > debian/control 
+ 
 # Remove the example files
 rm debian/*.ex
 rm debian/*.EX
